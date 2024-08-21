7db8cd787ee63539b7d6e720d72d76c92c4f2d13
p2r3 <p2r3@p2r3.com>
2024-08-21 22:36:23 +0300

    rename tempdir

--- globals.cpp
+++ globals.cpp
@@ -2,6 +2,6 @@
 #include "globals.h"
 
 // Points to the system-specific designated temporary file path
-const std::filesystem::path TEMP_DIR = std::filesystem::temp_directory_path() / "cpplice";
+const std::filesystem::path TEMP_DIR = std::filesystem::temp_directory_path() / "spplice-cpp";
 // Holds the current package installation state
 int SPPLICE_INSTALL_STATE = 0; // 0 - idle; 1 - installing; 2 - installed
