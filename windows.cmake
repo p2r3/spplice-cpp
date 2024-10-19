5b69cec2fcbe542b2fb245e1e738ff8967ebd2da
p2r3 <p2r3@p2r3.com>
2024-10-19 20:49:59 +0300

    remove cmd window on startup on windows

--- windows.cmake
+++ windows.cmake
@@ -6,6 +6,9 @@ set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
+# Gets rid of the console window at startup
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mwindows")
+
 # Set the Qt directory
 set(CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/qt5build/win32")
 
